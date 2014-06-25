b27404fc43d5a6850bf263e2c77d63b24b1f9cd1
jsbain <jsbain@github>
2014-06-25 04:59:01 +0000

    added comment/uncomment

--- editmenu.py
+++ editmenu.py
@@ -38,6 +38,33 @@ def unindent():
 
 	editor.set_selection(i[0],i[1]-len(t)+len(editor.get_text()))
 
+def comment():
+	"""" comment out selected lines"""
+	import editor
+	import re
+	COMMENT='#' 
+	i=editor.get_line_selection()
+	t=editor.get_text()
+	# replace every occurance of newline with  ewline plus COMMENT, except last newline
+	editor.replace_text(i[0],i[1]-1,COMMENT+re.sub(r'\n',r'\n'+COMMENT,t[i[0]:i[1]-1]))
+
+	editor.set_selection(i[0],i[1]-len(t)+len(editor.get_text()))
+
+def uncomment():
+	"""" uncomment selected lines"""
+	import editor
+	import re
+	COMMENT='#' 
+	i=editor.get_line_selection()
+	t=editor.get_text()
+# replace every occurance of newline # with newline, except last newline	
+#  todo.. probably should verify every line has comment...
+#  num lines= re.findall
+
+	if all( [x.startswith('#') for x in t[i[0]:i[1]-1].split(r'\n')]):
+		editor.replace_text(i[0],i[1]-1,re.sub(r'^'+COMMENT,r'',t[i[0]:i[1]-1],flags=re.MULTILINE))
+
+	editor.set_selection(i[0],i[1]-len(t)+len(editor.get_text()))
 
 def execlines():
 	"""execute selected lines in console.	""" 
