cb48f1bfea6974bfd7e6644aefe8da264d63d27a
jsbain <jsbain>
2014-10-19 06:21:36 +0000

    force scroll to selection

--- Find_and_replace.py
+++ Find_and_replace.py
@@ -30,18 +30,21 @@ def seg(sender):
 def select_text():
     full_text = editor.get_text()
     marker = full_text.find(find_text)
+    force_scroll(marker)
     editor.set_selection(marker, marker + len(find_text))
     
 def select_next(sender):
     full_text = editor.get_text()
     marker = editor.get_selection()[1]
     start = full_text.find(find_text, marker)
+    force_scroll(start)
     editor.set_selection(start, start + len(find_text))
     
 def select_previous(sender):
     full_text = editor.get_text()
     marker = editor.get_selection()[0]
     start = full_text.rfind(find_text, 0, marker)
+    force_scroll(start)
     editor.set_selection(start, start + len(find_text))
 
 def replace_it(sender):
@@ -55,6 +58,23 @@ def replace_it(sender):
         full_text = editor.get_text()
         full_replacement = full_text.replace(find_text, replacement)
         editor.replace_text(0, len(full_text), full_replacement)
+        
+def line_from_offset(start):
+    return editor.get_text()[0:start].count('\n')+1
+
+def offset_from_line(line):
+    lines=editor.get_text().splitlines(True)[0:line]
+    return sum([len(s) for s in lines])
+    
+def force_scroll(offset):
+    line = line_from_offset(offset)
+    if line > 10:
+        scrollline = line -10
+    else:
+        scrollline = line
+    scrolloffset = offset_from_line(line)
+    editor.replace_text(scrolloffset,scrolloffset,'')
+    
 def showfindbar():
 	v = ui.load_view('Find_and_replace')
 	
